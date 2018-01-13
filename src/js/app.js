dd35c0ac8a9ba1918aee603b0b869e26f20f5c37
Douglas Maitelli <dougmaitelli@gmail.com>
2018-01-13 19:48:07 +0100

    Deploy gh-pages

--- src/js/app.js
+++ src/js/app.js
@@ -4,56 +4,66 @@ import $ from "jquery";
 import Rainbow from "rainbowvis.js";
 
 $.fn.random = function() {
-    var randomIndex = Math.floor(Math.random() * this.length);
-    return $(this[randomIndex]);
+  var randomIndex = Math.floor(Math.random() * this.length);
+  return $(this[randomIndex]);
 };
 
-var docWidth = $(document).width();
-var docHeight = $(document).height();
+let getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
-var nWidth = 25;
-var nHeight = 20;
+const docWidth = $(document).width();
+const docHeight = $(document).height();
 
-var xSteps = Math.floor(docWidth / nWidth);
-var ySteps = Math.floor(docHeight / nHeight);
+let nWidth = 25;
+let nHeight = 20;
+
+let xSteps = Math.floor(docWidth / nWidth);
+let ySteps = Math.floor(docHeight / nHeight);
 
 nWidth += (docWidth % nWidth) / xSteps;
 nHeight += (docHeight % nHeight) / ySteps;
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
 var rainbow = new Rainbow();
 rainbow.setNumberRange(0, xSteps);
 rainbow.setSpectrum("#42f445", "#8341f4");
 
-var x = 0;
+let x = 0;
 do {
-  var y = 0;
+  let y = 0;
   do {
-    var opacity = Math.random();
+    let opacity = Math.random();
 
     if (Math.random() < 0.7 && opacity > 0.1) {
-      var n = $("<div/>").text(("00" + getRandomInt(0, 255).toString(16)).toUpperCase().slice(-2)).addClass("number").css({
-        left: (x * nWidth) + "px",
-        top: (y * nHeight) + "px",
-        color: "#" + rainbow.colourAt(x),
-        opacity: opacity
-      });
+      let n = $("<div/>")
+        .text(
+          ("00" + getRandomInt(0, 255).toString(16)).toUpperCase().slice(-2)
+        )
+        .addClass("number")
+        .css({
+          left: x * nWidth + "px",
+          top: y * nHeight + "px",
+          color: "#" + rainbow.colourAt(x),
+          opacity: opacity
+        });
       n.appendTo(".matrix");
     }
 
     y++;
-  } while(y < ySteps);
+  } while (y < ySteps);
 
   x++;
-} while(x < xSteps);
+} while (x < xSteps);
 
-function pulsate() {
-  $(".number").random().animate({ opacity: "+=0.3" }, 500, 'linear')
-    .animate({ opacity: "-=0.3" }, 500, 'linear');
-}
+let pulsate = () => {
+  for (var i = 0; i < 20; i++) {
+    $(".number")
+      .random()
+      .animate({ opacity: "+=0.3" }, 1000, "linear")
+      .animate({ opacity: "-=0.3" }, 1000, "linear");
+  }
+};
+
 $(document).ready(function() {
-  setInterval(pulsate, 5);
+  setInterval(pulsate, 50);
 });
