46d6700d560cb4e16e16b9529840f07efd9a53a3
Douglas Maitelli <douglas.maitelli@sap.com>
2018-01-15 11:23:56 +0100

    Lib build

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,87 @@
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  entry: {
-    "lib/digitalmatrix": "./src/js/digitalmatrix.js",
-    "app.bundle": "./src/js/app.js",
-    "app.bundle.min": "./src/js/app.js"
-  },
-  devtool: "source-map",
-  output: {
-    path: __dirname + "/build",
-    filename: "[name].js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        use: {
-          loader: "url-loader"
-        }
-      },
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["env"]
+module.exports = [
+  {
+    entry: {
+      "digitalmatrix": "./src/js/digitalmatrix.js",
+      "digitalmatrix.min": "./src/js/digitalmatrix.js"
+    },
+    devtool: "source-map",
+    output: {
+      path: __dirname + "/lib",
+      filename: "[name].js",
+      library: "digitalmatrix",
+      libraryTarget: 'umd',
+      umdNamedDefine: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["env"]
+            }
           }
         }
-      }
+      ]
+    },
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        include: /\.min\.js$/,
+        minimize: true
+      })
     ]
   },
-  plugins: [
-    /*new webpack.optimize.UglifyJsPlugin({
-      include: /\.min\.js$/,
-      minimize: true
-    }),*/
-    new HtmlWebpackPlugin({
-      chunks: ["app.bundle.min"],
-      template: "./src/index.html"
-    })
-  ],
-  devServer: {
-    inline: true
+  {
+    entry: {
+      "app.bundle": "./src/js/app.js",
+      "app.bundle.min": "./src/js/app.js"
+    },
+    devtool: "source-map",
+    output: {
+      path: __dirname + "/build",
+      filename: "[name].js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+        },
+        {
+          test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+          use: {
+            loader: "url-loader"
+          }
+        },
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["env"]
+            }
+          }
+        }
+      ]
+    },
+    plugins: [
+      /*new webpack.optimize.UglifyJsPlugin({
+        include: /\.min\.js$/,
+        minimize: true
+      }),*/
+      new HtmlWebpackPlugin({
+        chunks: ["app.bundle.min"],
+        template: "./src/index.html"
+      })
+    ],
+    devServer: {
+      inline: true
+    }
   }
-};
+];
