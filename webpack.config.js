e1264dc8339066ab38d1b67f36221cbb08a6ebf0
Douglas Maitelli <dougmaitelli@gmail.com>
2018-08-20 16:58:24 +0200

    Updated Deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,8 @@
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
 module.exports = [
   {
     entry: {
-      digitalmatrix: "./src/js/digitalmatrix.js",
       "digitalmatrix.min": "./src/js/digitalmatrix.js"
     },
-    devtool: "source-map",
     output: {
       path: __dirname + "/lib",
       filename: "[name].js",
@@ -15,6 +10,7 @@ module.exports = [
       libraryTarget: "umd",
       umdNamedDefine: true
     },
+    devtool: "source-map",
     module: {
       rules: [
         {
@@ -32,54 +28,6 @@ module.exports = [
           }
         }
       ]
-    },
-    plugins: [
-      new webpack.optimize.UglifyJsPlugin({
-        include: /\.min\.js$/,
-        minimize: true
-      })
-    ]
-  },
-  {
-    entry: {
-      "app.bundle": "./src/js/app.js",
-      "app.bundle.min": "./src/js/app.js"
-    },
-    devtool: "source-map",
-    output: {
-      path: __dirname + "/build",
-      filename: "[name].js"
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: [{ loader: "style-loader" }, { loader: "css-loader" }]
-        },
-        {
-          test: /\.js$/,
-          exclude: /(node_modules)/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              presets: ["env"]
-            }
-          }
-        }
-      ]
-    },
-    plugins: [
-      /*new webpack.optimize.UglifyJsPlugin({
-        include: /\.min\.js$/,
-        minimize: true
-      }),*/
-      new HtmlWebpackPlugin({
-        chunks: ["app.bundle.min"],
-        template: "./src/index.html"
-      })
-    ],
-    devServer: {
-      inline: true
     }
   }
 ];
